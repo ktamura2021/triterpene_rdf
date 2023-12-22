64934f267653e6a18514982727374796d3d0f059
ktamura2021 <tamurak@hiroshima-u.ac.jp>
2023-12-22 13:35:54 +0900

    change location of ontology.ttl

--- tmp/makerdf_ncbiprot_taxid.py
+++ tmp/makerdf_ncbiprot_taxid.py
@@ -5,7 +5,7 @@ file_name = 'v20231129_db02_ncbiprot_taxid.tsv'
 fp = open(file_name.replace('.tsv', '.ttl'), 'w')
 
 fp.write('@prefix up_taxonomy: <http://purl.uniprot.org/taxonomy/> .\n')
-fp.write('@prefix : <https://raw.githubusercontent.com/ktamura2021/triterpenoid_rdf/main/ontology.ttl#> .\n')
+fp.write('@prefix : <https://ktamura2021.github.io/triterpene_rdf/ontology.ttl#> .\n')
 
 num = 0
 with open(file_name, encoding='utf-8') as f:
