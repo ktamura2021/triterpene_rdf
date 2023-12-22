5f58cb2bb4af209b4143da35cd13e5eb7e4b3c43
Hirokazu Chiba <hchiba1@gmail.com>
2023-12-22 11:41:30 +0900

    Modify sparql.js to fetch fasta

--- site/sparql.js
+++ site/sparql.js
@@ -32,7 +32,58 @@ WHERE {
       :order ?order ;
       :species ?species ;
       :pubmed ?pubmed .
-}   
+}
+`;
+
+  try {
+    const response = await fetch(endpointUrl, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/sparql-results+json',
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: `query=${encodeURIComponent(sparqlQuery)}`
+    });
+    if (!response.ok) {
+      // response.ok === true if the status code is 2xx
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error(`Could not fetch data: ${error}`);
+  }
+}
+
+async function fetchFastaBySPARQL(selectedType, selectedPathway, selectedOrder) {
+  const endpointUrl = 'https://spang.dbcls.jp/sparql';
+  let valuesForType = '';
+  if (selectedType) {
+    valuesForType = `VALUES (?type) { ("${selectedType}") }`;
+  }
+  let valuesForPathway = '';
+  if (selectedPathway) {
+    valuesForPathway = `VALUES (?pathway) { ("${selectedPathway}") }`;
+  }
+  let valuesForOrder = '';
+  if (selectedOrder) {
+    valuesForOrder = `VALUES (?order) { ("${selectedOrder}") }`;
+  }
+  const sparqlQuery = `
+PREFIX up: <http://purl.uniprot.org/uniprot/>
+PREFIX pubmed: <https://pubmed.ncbi.nlm.nih.gov/>
+PREFIX : <https://raw.githubusercontent.com/ktamura2021/triterpenoid_rdf/main/ontology.ttl#>
+
+SELECT ?ID ?type ?name ?sequence
+WHERE {
+  ${valuesForType}
+  ${valuesForPathway}
+  ${valuesForOrder}
+  ?s  :type ?type ;
+      :ID ?ID ;
+      :name ?name ;
+      :aa_sequence ?sequence .
+}
 `;
 
   try {
