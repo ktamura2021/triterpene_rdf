4474b91ced4185201d0decf6c77e5289e655f1c3
ktamura2021 <tamurak@hiroshima-u.ac.jp>
2024-02-15 16:32:04 +0900

    update dbfinal.tsv

--- tmp/create_dbfinal.py
+++ tmp/create_dbfinal.py
@@ -10,12 +10,15 @@ def read_tsv(file_path):
 
 table1 = read_tsv('v20231129_db03.tsv')
 table2 = read_tsv('v20231224_db03.tsv')
+table3 = read_tsv('v20240207_db03.tsv')
 
-table_concat = table1 + table2
+# table_concat = table1 + table2 # v20231224
+table_concat = table1 + table2 + table3
 
 # export
-ver = 'v20231224'
-output_file_path = ver + '_dbfinal.tsv'
+# ver = 'v20231224'
+# output_file_path = ver + '_dbfinal.tsv' # v20231224
+output_file_path = 'dbfinal.tsv'
 header = table_concat[0].keys() if table_concat else []
 
 with open(output_file_path, 'w', newline='', encoding='utf-8') as output_file:
